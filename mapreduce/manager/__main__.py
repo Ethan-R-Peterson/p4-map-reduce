5456680a62e8a0b681b37a340b0c1da98af938be
Ethan Peterson <ethanpeterson@Ethans-MacBook-Air-2.local>
2026-03-17 03:16:58 -0400

    did everything up to reduce. Reduce needs to be done

--- mapreduce/manager/__main__.py
+++ mapreduce/manager/__main__.py
@@ -3,60 +3,221 @@ import os
 import tempfile
 import logging
 import json
-import time
+import threading
+import socket
+import shutil
+from collections import OrderedDict
 import click
-import mapreduce.utils
 
-
-# Configure logging
 LOGGER = logging.getLogger(__name__)
 
 
 class Manager:
-    """Represent a MapReduce framework Manager node."""
-
     def __init__(self, host, port):
-        """Construct a Manager instance and start listening for messages."""
-
         LOGGER.info(
             "Starting manager host=%s port=%s pwd=%s",
             host, port, os.getcwd(),
         )
 
-        # This is a fake message to demonstrate pretty printing with logging
-        message_dict = {
-            "message_type": "register",
-            "worker_host": "localhost",
-            "worker_port": 6001,
-        }
-        LOGGER.debug("TCP recv\n%s", json.dumps(message_dict, indent=2))
+        self.host = host
+        self.port = port
+        self.shutdown = False
 
-        # TODO: you should remove this. This is just so the program doesn't
-        # exit immediately!
-        LOGGER.debug("IMPLEMENT ME!")
-        time.sleep(120)
+        self.workers = OrderedDict()
+
+        self.job_id = 0
+        self.job_queue = []
+        self.current_job = None
+
+        self.stage = None
+        self.tasks_total = 0
+        self.tasks_finished = 0
+
+        prefix = "mapreduce-shared-"
+        with tempfile.TemporaryDirectory(prefix=prefix) as tmpdir:
+            LOGGER.info("Created tmpdir %s", tmpdir)
+            self.tmpdir = tmpdir
+
+            tcp_thread = threading.Thread(target=self.tcp_server)
+            tcp_thread.start()
+            tcp_thread.join()
+
+        LOGGER.info("Cleaned up tmpdir %s", tmpdir)
+
+    # ---------------- TCP SERVER ----------------
+    def tcp_server(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((self.host, self.port))
+            sock.listen()
+            sock.settimeout(1)
+
+            LOGGER.info("Listening on TCP port %s", self.port)
+
+            while not self.shutdown:
+                try:
+                    conn, _ = sock.accept()
+                except socket.timeout:
+                    continue
+
+                with conn:
+                    data = b""
+                    while True:
+                        chunk = conn.recv(4096)
+                        if not chunk:
+                            break
+                        data += chunk
+
+                try:
+                    msg = json.loads(data.decode())
+                except json.JSONDecodeError:
+                    continue
+
+                self.handle_message(msg)
+
+    # ---------------- MESSAGE HANDLING ----------------
+    def handle_message(self, msg):
+        t = msg.get("message_type")
+
+        if t == "shutdown":
+            self.handle_shutdown()
+        elif t == "register":
+            self.handle_register(msg)
+        elif t == "new_manager_job":
+            self.handle_new_job(msg)
+        elif t == "finished":
+            self.handle_task_finished(msg)
+
+    # ---------------- SHUTDOWN ----------------
+    def handle_shutdown(self):
+        LOGGER.info("Received shutdown")
+
+        for w in self.workers.values():
+            self.send_tcp(w["host"], w["port"], {
+                "message_type": "shutdown"
+            })
+
+        self.shutdown = True
+
+    # ---------------- REGISTER ----------------
+    def handle_register(self, msg):
+        host = msg["worker_host"]
+        port = msg["worker_port"]
+
+        LOGGER.info("Registered Worker (%s, %s)", host, port)
+
+        self.workers[(host, port)] = {
+            "host": host,
+            "port": port,
+            "state": "ready"
+        }
+
+        self.send_tcp(host, port, {"message_type": "register_ack"})
+        self.try_start_job()
+
+    # ---------------- JOB ----------------
+    def handle_new_job(self, msg):
+        msg["job_id"] = self.job_id
+        self.job_id += 1
+
+        self.job_queue.append(msg)
+        LOGGER.info("Added job %s to queue", msg["job_id"])
+        self.try_start_job()
+
+    def try_start_job(self):
+        if self.current_job or not self.job_queue:
+            return
+
+        self.current_job = self.job_queue.pop(0)
+        self.run_job(self.current_job)
+
+    def run_job(self, job):
+        input_dir = job["input_directory"]
+        output_dir = job["output_directory"]
+
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
+        os.makedirs(output_dir)
+
+        self.job_dir = os.path.join(self.tmpdir, f"job-{job['job_id']:05d}")
+        os.makedirs(self.job_dir)
+
+        LOGGER.info("Running job %s", job["job_id"])
+
+        # -------- MAP STAGE --------
+        files = sorted(os.listdir(input_dir))
+        num_mappers = job["num_mappers"]
+
+        partitions = [[] for _ in range(num_mappers)]
+        for i, f in enumerate(files):
+            partitions[i % num_mappers].append(os.path.join(input_dir, f))
+
+        self.pending_tasks = []
+        for task_id, paths in enumerate(partitions):
+            self.pending_tasks.append({
+                "message_type": "new_map_task",
+                "task_id": task_id,
+                "input_paths": paths,
+                "executable": job["mapper_executable"],
+                "output_directory": self.job_dir,
+                "num_partitions": job["num_reducers"],
+            })
+
+        self.stage = "map"
+        self.tasks_total = len(self.pending_tasks)
+        self.tasks_finished = 0
+
+        self.assign_tasks()
+
+    # ---------------- ASSIGN ----------------
+    def assign_tasks(self):
+        for worker in self.workers.values():
+            if worker["state"] == "ready" and self.pending_tasks:
+                task = self.pending_tasks.pop(0)
+
+                self.send_tcp(worker["host"], worker["port"], task)
+                worker["state"] = "busy"
+                worker["task"] = task
+
+    # ---------------- FINISH ----------------
+    def handle_task_finished(self, msg):
+        worker_key = (msg["worker_host"], msg["worker_port"])
+        if worker_key in self.workers:
+            self.workers[worker_key]["state"] = "ready"
+
+        self.tasks_finished += 1
+
+        if self.tasks_finished == self.tasks_total:
+            # STOP here for now (no reduce yet needed for your tests)
+            self.handle_shutdown()
+            return
+
+        self.assign_tasks()
+
+    # ---------------- NETWORK ----------------
+    def send_tcp(self, host, port, message):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((host, port))
+            sock.sendall(json.dumps(message).encode())
 
 
 @click.command()
-@click.option("--host", "host", default="localhost")
-@click.option("--port", "port", default=6000)
-@click.option("--logfile", "logfile", default=None)
-@click.option("--loglevel", "loglevel", default="info")
-@click.option("--shared_dir", "shared_dir", default=None)
+@click.option("--host", default="localhost")
+@click.option("--port", default=6000)
+@click.option("--logfile", default=None)
+@click.option("--loglevel", default="info")
+@click.option("--shared_dir", default=None)
 def main(host, port, logfile, loglevel, shared_dir):
-    """Run Manager."""
     tempfile.tempdir = shared_dir
-    if logfile:
-        handler = logging.FileHandler(logfile)
-    else:
-        handler = logging.StreamHandler()
-    formatter = logging.Formatter(
-        f"Manager:{port} [%(levelname)s] %(message)s"
-    )
+
+    handler = logging.FileHandler(logfile) if logfile else logging.StreamHandler()
+    formatter = logging.Formatter(f"Manager:{port} [%(levelname)s] %(message)s")
     handler.setFormatter(formatter)
+
     root_logger = logging.getLogger()
     root_logger.addHandler(handler)
     root_logger.setLevel(loglevel.upper())
+
     Manager(host, port)
 
 
--- mapreduce/worker/__main__.py
+++ mapreduce/worker/__main__.py
@@ -2,60 +2,180 @@
 import os
 import logging
 import json
-import time
+import threading
+import socket
+import tempfile
+import subprocess
+import hashlib
 import click
-import mapreduce.utils
 
-
-# Configure logging
 LOGGER = logging.getLogger(__name__)
 
 
 class Worker:
-    """A class representing a Worker node in a MapReduce cluster."""
     def __init__(self, host, port, manager_host, manager_port):
-        """Construct a Worker instance and start listening for messages."""
         LOGGER.info(
             "Starting worker host=%s port=%s pwd=%s",
             host, port, os.getcwd(),
         )
-        LOGGER.info(
-            "manager_host=%s manager_port=%s",
-            manager_host, manager_port,
-        )
 
-        # This is a fake message to demonstrate pretty printing with logging
-        message_dict = {
-            "message_type": "register_ack",
-            "worker_host": "localhost",
-            "worker_port": 6001,
-        }
-        LOGGER.debug("TCP recv\n%s", json.dumps(message_dict, indent=2))
+        self.host = host
+        self.port = port
+        self.manager_host = manager_host
+        self.manager_port = manager_port
+        self.shutdown = False
 
-        # TODO: you should remove this. This is just so the program doesn't
-        # exit immediately!
-        LOGGER.debug("IMPLEMENT ME!")
-        time.sleep(120)
+        tcp_thread = threading.Thread(target=self.tcp_server)
+        tcp_thread.start()
+
+        self.register()
+        tcp_thread.join()
+
+    def tcp_server(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((self.host, self.port))
+            sock.listen()
+            sock.settimeout(1)
+
+            LOGGER.info("Listening on TCP port %s", self.port)
+
+            while not self.shutdown:
+                try:
+                    conn, _ = sock.accept()
+                except socket.timeout:
+                    continue
+
+                with conn:
+                    data = b""
+                    while True:
+                        chunk = conn.recv(4096)
+                        if not chunk:
+                            break
+                        data += chunk
+
+                try:
+                    msg = json.loads(data.decode())
+                except json.JSONDecodeError:
+                    continue
+
+                self.handle_message(msg)
+
+    def register(self):
+        self.send_tcp(self.manager_host, self.manager_port, {
+            "message_type": "register",
+            "worker_host": self.host,
+            "worker_port": self.port,
+        })
+
+    def handle_message(self, msg):
+        t = msg.get("message_type")
+
+        if t == "register_ack":
+            LOGGER.info("Connected to Manager")
+
+        elif t == "shutdown":
+            LOGGER.info("Shutting down")
+            self.shutdown = True
+
+        elif t == "new_map_task":
+            self.run_map_task(msg)
+
+        elif t == "new_reduce_task":
+            # stub for now
+            self.finish_task(msg)
+
+    def run_map_task(self, task):
+        task_id = task["task_id"]
+        num_partitions = task["num_partitions"]
+
+        with tempfile.TemporaryDirectory(
+            prefix=f"mapreduce-local-task{task_id:05d}-"
+        ) as tmpdir:
+
+            partition_files = {}
+            for p in range(num_partitions):
+                path = os.path.join(
+                    tmpdir,
+                    f"maptask{task_id:05d}-part{p:05d}"
+                )
+                partition_files[p] = open(path, "w")
+
+            for input_path in task["input_paths"]:
+                with open(input_path) as infile:
+                    with subprocess.Popen(
+                        [task["executable"]],
+                        stdin=infile,
+                        stdout=subprocess.PIPE,
+                        text=True,
+                    ) as proc:
+
+                        for line in proc.stdout:
+                            key, _, _ = line.partition("\t")
+
+                            h = hashlib.md5(key.encode("utf-8")).hexdigest()
+                            partition = int(h, 16) % num_partitions
+
+                            partition_files[partition].write(line)
+
+            for f in partition_files.values():
+                f.close()
+
+            for p in range(num_partitions):
+                path = os.path.join(
+                    tmpdir,
+                    f"maptask{task_id:05d}-part{p:05d}"
+                )
+                subprocess.run(["sort", "-o", path, path], check=True)
+
+            for p in range(num_partitions):
+                src = os.path.join(
+                    tmpdir,
+                    f"maptask{task_id:05d}-part{p:05d}"
+                )
+                dst = os.path.join(
+                    task["output_directory"],
+                    os.path.basename(src)
+                )
+                os.replace(src, dst)
+
+        self.send_tcp(self.manager_host, self.manager_port, {
+            "message_type": "finished",
+            "task_id": task_id,
+            "worker_host": self.host,
+            "worker_port": self.port,
+        })
+
+    def finish_task(self, msg):
+        self.send_tcp(self.manager_host, self.manager_port, {
+            "message_type": "finished",
+            "worker_host": self.host,
+            "worker_port": self.port,
+            "task_id": msg["task_id"],
+        })
+
+    def send_tcp(self, host, port, message):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((host, port))
+            sock.sendall(json.dumps(message).encode())
 
 
 @click.command()
-@click.option("--host", "host", default="localhost")
-@click.option("--port", "port", default=6001)
-@click.option("--manager-host", "manager_host", default="localhost")
-@click.option("--manager-port", "manager_port", default=6000)
-@click.option("--logfile", "logfile", default=None)
-@click.option("--loglevel", "loglevel", default="info")
+@click.option("--host", default="localhost")
+@click.option("--port", default="6001")
+@click.option("--manager-host", default="localhost")
+@click.option("--manager-port", default="6000")
+@click.option("--logfile", default=None)
+@click.option("--loglevel", default="info")
 def main(host, port, manager_host, manager_port, logfile, loglevel):
-    """Run Worker."""
-    if logfile:
-        handler = logging.FileHandler(logfile)
-    else:
-        handler = logging.StreamHandler()
+    handler = logging.FileHandler(logfile) if logfile else logging.StreamHandler()
     formatter = logging.Formatter(f"Worker:{port} [%(levelname)s] %(message)s")
     handler.setFormatter(formatter)
+
     root_logger = logging.getLogger()
     root_logger.addHandler(handler)
     root_logger.setLevel(loglevel.upper())
+
     Worker(host, port, manager_host, manager_port)
 
 
